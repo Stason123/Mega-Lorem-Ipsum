16d81cd6a2a8d59784f4378a4b8dafcfda5365d4
Stason123 <50232123+Stason123@users.noreply.github.com>
2021-02-26 20:34:24 +0200

    Changes for frontend requets to backend

---
+++ backend/RestAPI/RestAPI/Model/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Model
+{
+    public class TestClass
+    {
+        public int id { get; set; }
+    }
+}
