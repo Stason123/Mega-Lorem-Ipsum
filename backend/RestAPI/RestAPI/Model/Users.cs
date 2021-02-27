01d0a34a626750916514f2fcff12f4ae7a34c905
Stason123 <50232123+Stason123@users.noreply.github.com>
2021-02-27 13:41:48 +0200

    Changes to make possible display data from database on frontend

--- backend/RestAPI/RestAPI/Model/Users.cs
+++ backend/RestAPI/RestAPI/Model/Users.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace RestAPI.Model
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
+        [DisplayFormat(DataFormatString = "{yyyy-MM-dd}")]
         public DateTime? DateOfBirth { get; set; }
         public string Gender { get; set; }
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,31 @@
-'use strict';
-
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
-}
-
-counter();
-
-const requestUrlGet = 'https://localhost:44394/weatherforecast/Get';
-const requestUrlPost = 'https://localhost:44394/weatherforecast/Post';
 
 
+// function counter() {
+//   let seconds = 0;
+//   setInterval(() => {
+//     seconds += 1;
+//     document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
+//   }, 1000);
+// }
+
+// counter();
+
+// sendRequest('GET', requestUrlGet)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// const body = {
+//   id: 10
+// }
+
+// sendRequest('POST', requestUrlPost, body)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+import User from './Models/user.js';
+
+const restApiUrl = 'https://localhost:44394/';
+const table = document.getElementById("tableContainer");
 
 function sendRequest(method, url, body = null) {
   return new Promise((resolve, reject) => {
@@ -33,14 +45,24 @@ function sendRequest(method, url, body = null) {
   })
 }
 
-sendRequest('GET', requestUrlGet)
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
 
-const body = {
-  id: 10
-}
 
-sendRequest('POST', requestUrlPost, body)
-  .then(data => console.log(data))
+sendRequest('GET', restApiUrl+'users/getusers')
+  .then(data => {
+    data.forEach(element => {
+        const newUser = new User(element);
+        const lastRow = table.rows.length;
+        var row = table.insertRow(lastRow);
+        let i = 0;
+        for (const [key, value] of Object.entries(newUser)) {
+          var cell = row.insertCell(i);
+          if (key === 'dateOfBirth') {            
+            cell.innerHTML = `<input type="date" value="${value}" />`;
+          } else {
+            cell.innerHTML = `<input type="text" value="${value}" />`;
+          }
+          i++;
+        }
+    });
+  })
   .catch(err => console.log(err));
